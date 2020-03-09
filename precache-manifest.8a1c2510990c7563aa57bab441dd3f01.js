00da179fe9d9cf35576b4a80ef28f272eab4c33e
David Zetterdahl <david.zetterdahl@hiq.se>
2020-03-09 12:39:11 +0100

    Updates

---
+++ precache-manifest.8a1c2510990c7563aa57bab441dd3f01.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15f34e30917d0e18361ce4acc34e1c3",
+    "url": "/automat/index.html"
+  },
+  {
+    "revision": "fae5c8c977ad61b3ef25",
+    "url": "/automat/static/css/main.6d9ae8f6.chunk.css"
+  },
+  {
+    "revision": "ae0f823c1d79eab3876f",
+    "url": "/automat/static/js/2.48a08194.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/automat/static/js/2.48a08194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fae5c8c977ad61b3ef25",
+    "url": "/automat/static/js/main.da6e568d.chunk.js"
+  },
+  {
+    "revision": "760a3dfce6e1b193412a",
+    "url": "/automat/static/js/runtime-main.ef900d92.js"
+  },
+  {
+    "revision": "15c74cfbe4d77893710c0b16bc9635cd",
+    "url": "/automat/static/media/aoe2.15c74cfb.jpg"
+  },
+  {
+    "revision": "95335645c64efaf0719ae5b2ec0d3084",
+    "url": "/automat/static/media/bms.95335645.jpg"
+  },
+  {
+    "revision": "ae1d04a1e22aa8982791c1a0d2afe41b",
+    "url": "/automat/static/media/ctop.ae1d04a1.jpg"
+  },
+  {
+    "revision": "6b780ab53159f96de4f78aaedf5be489",
+    "url": "/automat/static/media/hl2.6b780ab5.jpg"
+  },
+  {
+    "revision": "23181d39fbe846e0a77d7486c21879b3",
+    "url": "/automat/static/media/menofwar.23181d39.jpg"
+  },
+  {
+    "revision": "4ee657cb29c597bee89928789c18f42a",
+    "url": "/automat/static/media/sc2.4ee657cb.jpg"
+  },
+  {
+    "revision": "a209d0fc9e68a1810502752135441440",
+    "url": "/automat/static/media/sc2a.a209d0fc.jpg"
+  },
+  {
+    "revision": "0db3df4c3f53de4a3595d24d092cff5f",
+    "url": "/automat/static/media/steve.0db3df4c.jpg"
+  },
+  {
+    "revision": "a3c26647e4cb991966f8e072b9bc62d0",
+    "url": "/automat/static/media/tortoiseisland.a3c26647.jpg"
+  },
+  {
+    "revision": "16e8c66f12c06b48df483f3f9679f78c",
+    "url": "/automat/static/media/wunderling.16e8c66f.jpg"
+  },
+  {
+    "revision": "aa5357433c61c3fee6476a3a4f9a4af0",
+    "url": "/automat/static/media/wunderling_a.aa535743.jpg"
+  }
+]);
